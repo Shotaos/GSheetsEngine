bdc163079363a8e7f996d5b57ed05a84c9855076
Shota Osepashvili <ShotaosephashvilI@gmail.com>
2021-05-27 13:07:56 -0400

    Add Gservice login check function

--- sheets.py
+++ sheets.py
@@ -1,25 +1,56 @@
 import os.path
 import re
+from pathlib import Path
+from pprint import pprint
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 
+TOKEN_DIR= Path.home() / ".sheetsearch"
+TOKEN_DIR.mkdir(exist_ok=True)
+
+
 class GoogleSheets():
-	SPREADSHEET_ID = '11IETLAULJA1NpDuRWp7WtnNbvAhIcJKuJNJ-N6vYMm4'
 	SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/documents']
 
-	def __init__(self):
+	def __init__(self, spreadsheet_id='11IETLAULJA1NpDuRWp7WtnNbvAhIcJKuJNJ-N6vYMm4'):
+		self.spreadsheet_id = spreadsheet_id
+		self.creds = None
+
+	def check_credentials(self):
+		if os.path.exists(TOKEN_DIR / 'token.json'):
+		    creds = Credentials.from_authorized_user_file(TOKEN_DIR / 'token.json', self.SCOPES)
+		else:
+			return False
+
+		# If there are no (valid) credentials available, let the user log in.
+		if not creds or not creds.valid:
+			if creds and creds.expired and creds.refresh_token:
+				creds.refresh(Request())
+
+				# Check validity after refresh
+				if not creds.valid:
+					return False
+			else:
+				return False
+		return True
+
+	def login(self):
 		self.__init_service()
+	
+	def __check_creds_validity(self):
+		if not self.creds or not self.creds.valid:
+			self.__init_service()
 
 	def __init_service(self):
 		creds = None
 		# The file token.json stores the user's access and refresh tokens, and is
 		# created automatically when the authorization flow completes for the first
 		# time.
-		if os.path.exists('token.json'):
-		    creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+		if os.path.exists(TOKEN_DIR / 'token.json'):
+		    creds = Credentials.from_authorized_user_file(TOKEN_DIR / 'token.json', self.SCOPES)
 
 		# If there are no (valid) credentials available, let the user log in.
 		if not creds or not creds.valid:
@@ -34,31 +65,39 @@ class GoogleSheets():
 		    with open('token.json', 'w') as token:
 			    token.write(creds.to_json())
 
+		self.creds = creds
 		self.docs = build('docs', 'v1', credentials=creds).documents()
 		self.sheets = build('sheets', 'v4', credentials=creds).spreadsheets()
 
 	def get_sheet_info(self):
-		response = self.sheets.get(spreadsheetId=self.SPREADSHEET_ID, fields='sheets/properties').execute()
+		self.__check_creds_validity()
+
+		response = self.sheets.get(spreadsheetId=self.spreadsheet_id, fields='sheets/properties').execute()
 		return [sheet['properties'] for sheet in response['sheets']]
 
 	def get_sheet_names(self):
+		self.__check_creds_validity()
+
 		sheets = self.get_sheet_info()
 		return [sheet['title'] for sheet in sheets]
 
 	def get_sheet_data(self, sheet):
-		values = self.sheets.values().get(spreadsheetId=self.SPREADSHEET_ID, range=sheet + '!A:C').execute()
+		self.__check_creds_validity()
+
+		values = self.sheets.values().get(spreadsheetId=self.spreadsheet_id, range=sheet + '!A:C').execute()
 		return values['values'][1:]
 
 	
 	def search(self, query, sheets=[]):
-		result = []
+		self.__check_creds_validity()
 
+		result = []
 		# Get sheet names
 		sheets = sheets if sheets else self.get_sheet_names()
 		# Build the ranges expression ["sheet1!A:C", "sheet2!A:C", "sheet3!A:C"]
 		ranges = [sheet + '!A:C' for sheet in sheets]
 
-		values = self.sheets.values().batchGet(spreadsheetId=self.SPREADSHEET_ID, ranges=ranges).execute()
+		values = self.sheets.values().batchGet(spreadsheetId=self.spreadsheet_id, ranges=ranges).execute()
 
 		for sheet in values['valueRanges']:
 			sheet_name = sheet['range'].split('!')[0]
@@ -69,9 +108,11 @@ class GoogleSheets():
 		return result
 
 	def insert_row(self, sheet, row):
+		self.__check_creds_validity()
+
 		body = {'majorDimension': 'ROWS',
 			'values': [row]}
-		request = self.sheets.values().append(spreadsheetId=self.SPREADSHEET_ID,
+		request = self.sheets.values().append(spreadsheetId=self.spreadsheet_id,
 							valueInputOption="RAW",
 							range=sheet + '!A:C',
 							body=body)
@@ -79,5 +120,7 @@ class GoogleSheets():
 		
 
 	def create_doc(self, name):
+		self.__check_creds_validity()
+
 		response =  self.docs.create(body={'title' : name}).execute()
 		return "https://docs.google.com/document/d/" + response['documentId'] + "/edit"
